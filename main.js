485e46561474c37d2461145d53c3cf0fd67db069
Avinash132009 <82804900+Avinash132009@users.noreply.github.com>
2021-04-19 14:09:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+function date1(){
+    document.getElementById("date").innerHTML="14 March 2021"
+}
+
+function date2(){
+    document.getElementById("date").innerHTML="4 March 2021"
+}
+
+function date3(){
+    document.getElementById("date").innerHTML="1 April 2021"
+}
+
+function date4(){
+    document.getElementById("date").innerHTML="19 March 2021"
+}
+
+function date5(){
+    document.getElementById("date").innerHTML="20 February 2021"
+}
+
+function date6(){
+    document.getElementById("date").innerHTML="17 March 2021"
+}
+
+function date7(){
+    document.getElementById("date").innerHTML="9 March 2021"
+}
